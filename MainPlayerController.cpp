f2f387f2c75bc0c0fbea97c1980667986d2e9ad9
Kyle Mobly <kmobly@gmail.com>
2022-04-10 21:43:34 -0700

    Add files via upload
    The rest of the cpp files for the Easter game.

---
+++ MainPlayerController.cpp
@@ -0,0 +1,105 @@
+
+
+#include "MainPlayerController.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Widget.h"
+
+AMainPlayerController::AMainPlayerController()
+{
+	bPauseMenuOpen = false;
+}
+
+void AMainPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HUDOverlayAsset)
+	{
+		HUDOverlay = CreateWidget<UUserWidget>(this, HUDOverlayAsset);
+	}
+	HUDOverlay->AddToViewport();
+	HUDOverlay->SetVisibility(ESlateVisibility::Visible);
+
+	if (WPauseMenu)
+	{
+		PauseMenu = CreateWidget<UUserWidget>(this, WPauseMenu);
+		if (PauseMenu)
+		{
+			PauseMenu->AddToViewport();
+			PauseMenu->SetVisibility(ESlateVisibility::Hidden);
+		}
+	}
+}
+
+void AMainPlayerController::DisplayEnemyHealthBar()
+{
+	if (EnemyHealthBar)
+	{
+		bEnemyHealthBarVisible = true;
+		EnemyHealthBar->SetVisibility(ESlateVisibility::Visible);
+	}
+}
+
+void AMainPlayerController::RemoveEnemyHealthBar()
+{
+	if (EnemyHealthBar)
+	{
+		bEnemyHealthBarVisible = false;
+		EnemyHealthBar->SetVisibility(ESlateVisibility::Hidden);
+	}
+}
+
+void AMainPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (EnemyHealthBar)
+	{
+		FVector2D PositionInViewport;
+		ProjectWorldLocationToScreen(EnemyLocation, PositionInViewport);
+		PositionInViewport.Y -= 85.f;
+
+		FVector2D SizeInViewport(300.f, 25.f);
+
+		EnemyHealthBar->SetPositionInViewport(PositionInViewport);
+		EnemyHealthBar->SetDesiredSizeInViewport(SizeInViewport);
+	}
+
+}
+
+void AMainPlayerController::DisplayPauseMenu_Implementation()
+{
+	bPauseMenuOpen = true;
+	bShowMouseCursor = true;
+
+	FInputModeGameAndUI InputModeGameAndUI;
+	SetInputMode(InputModeGameAndUI);
+
+}
+
+
+void AMainPlayerController::RemovePauseMenu_Implementation()
+{
+	bPauseMenuOpen = false;
+	bShowMouseCursor = false;
+
+	GameModeOnly();
+}
+
+void AMainPlayerController::TogglePauseMenu()
+{
+	if (!bPauseMenuOpen)
+	{
+		DisplayPauseMenu();
+	}
+	else
+	{
+		RemovePauseMenu();
+	}
+}
+
+void AMainPlayerController::GameModeOnly()
+{
+	FInputModeGameOnly InputModeGameOnly;
+	SetInputMode(InputModeGameOnly);
+}
